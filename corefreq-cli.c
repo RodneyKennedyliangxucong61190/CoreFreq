RodneyKennedyliangxucong61190/CoreFreq

commit 6a22bbda1b3671a0a6f61f04b0075a96f5a3da4e

[CLI] Pre-compute the right aligned position of Voltage & Temp in footer

corefreq-cli.c
@@ -5744,7 +5744,24 @@ struct DRAW_ST Draw = {
 	.View		= V_FREQ,
 	.Disposal	= D_MAINVIEW,
 	.Size		= { .width = 0, .height = 0 },
-	.Area		= { .MinHeight = 0, .MaxRows = 0, .LoadWidth = 0 },
+	.Area		= {
+				.MinHeight = 0, .MaxRows = 0, .LoadWidth = 0,
+	    #ifndef NO_FOOTER
+		.Footer = {
+			.VoltTemp = {
+				.Hot = {
+					[0] = MIN_WIDTH - 13,
+					[1] = MIN_WIDTH - 20
+				},
+			},
+			.TaskMem = {
+				.Total	= MIN_WIDTH - 35,
+				.Free	= MIN_WIDTH - 22,
+				.Count	= MIN_WIDTH - 12
+			},
+		},
+	    #endif
+	},
 	.iClock 	= 0,
 	.cpuScroll	= 0,
 	.Load		= 0,
@@ -13225,9 +13242,14 @@ void PrintTaskMemory(Layer *layer, CUINT row,
 			freeRAM >> Draw.Unit.Memory,
 			totalRAM >> Draw.Unit.Memory);
 
-	memcpy(&LayerAt(layer, code, (Draw.Size.width-35), row), &Buffer[0], 6);
-	memcpy(&LayerAt(layer, code, (Draw.Size.width-22), row), &Buffer[6], 9);
-	memcpy(&LayerAt(layer, code, (Draw.Size.width-12), row), &Buffer[15],9);
+	memcpy(&LayerAt(layer, code, Draw.Area.Footer.TaskMem.Total, row),
+			&Buffer[0], 6);
+
+	memcpy(&LayerAt(layer, code, Draw.Area.Footer.TaskMem.Free, row),
+			&Buffer[6], 9);
+
+	memcpy(&LayerAt(layer, code, Draw.Area.Footer.TaskMem.Count, row),
+			&Buffer[15], 9);
 }
 #endif
 
@@ -14004,6 +14026,11 @@ void Layout_Footer(Layer *layer, CUINT row)
 	LayerDeclare(	LAYOUT_FOOTER_TECH_X86, RSZ(LAYOUT_FOOTER_TECH_X86),
 			0, row, hTech0 );
 
+	LayerDeclare(	LAYOUT_FOOTER_VOLT_TEMP, RSZ(LAYOUT_FOOTER_VOLT_TEMP),
+			Draw.Size.width - 15, row, hVoltTemp0 );
+	/* Pre-compute right-aligned position of Voltage & Temperature items */
+	Draw.Area.Footer.VoltTemp.Hot[0] = hVoltTemp0.origin.col + 2;
+
 	const ATTRIBUTE EN[] = {
 		RSC(UI).ATTR()[UI_LAYOUT_FOOTER_ENABLE_0],
 		RSC(UI).ATTR()[UI_LAYOUT_FOOTER_ENABLE_1],
@@ -14094,6 +14121,8 @@ void Layout_Footer(Layer *layer, CUINT row)
 			(Draw.Size.width - hTech0.length - hTech1.length),
 			hSpace,
 			RSC(UI).ATTR()[UI_LAYOUT_FOOTER_FILL]);
+
+	Draw.Area.Footer.VoltTemp.Hot[1] = Draw.Area.Footer.VoltTemp.Hot[0] - 7;
     }
     else
     {
@@ -14142,9 +14171,15 @@ void Layout_Footer(Layer *layer, CUINT row)
 			(Draw.Size.width - hTech0.length - hTech1.length),
 			hSpace,
 			RSC(UI).ATTR()[UI_LAYOUT_FOOTER_FILL]);
+
+	Draw.Area.Footer.VoltTemp.Hot[1]=Draw.Area.Footer.VoltTemp.Hot[0] - 11;
       }
     }
-	LayerAt(layer, code, 14+64, row) = Setting.fahrCels ? 'F' : 'C';
+	LayerCopyAt(layer, hVoltTemp0.origin.col, hVoltTemp0.origin.row,
+			hVoltTemp0.length, hVoltTemp0.attr, hVoltTemp0.code);
+
+	LayerAt(layer, code, Draw.Area.Footer.VoltTemp.Hot[0] + 11, row) = \
+						Setting.fahrCels ? 'F' : 'C';
 
 	row++;
 
@@ -14206,6 +14241,10 @@ void Layout_Footer(Layer *layer, CUINT row)
 				can, ScrambleSMBIOS(Draw.SmbIndex, 4, '-'),
 				RSC(UI).ATTR()[UI_LAYOUT_FOOTER_DMI_STRING] );
 	}
+	/* Pre-compute position of Tasks count & Memory usage		*/
+	Draw.Area.Footer.TaskMem.Total	= Draw.Size.width - 35;
+	Draw.Area.Footer.TaskMem.Free	= Draw.Size.width - 22;
+	Draw.Area.Footer.TaskMem.Count	= Draw.Size.width - 12;
 	/* Reset Tasks count & Memory usage				*/
 	if (BITWISEAND(LOCKLESS, Shm->SysGate.Operation, 0x1)) {
 		PrintTaskMemory(layer, row,
@@ -16175,39 +16214,51 @@ void Draw_Footer(Layer *layer, CUINT row)
 		_tmp = 3;
 	    }
 	}
+	LayerAt(layer, attr,
+		Draw.Area.Footer.VoltTemp.Hot[1], row) = eventAttr[_hot][0];
 
-	if (Shm->Proc.Features.Info.Vendor.CRC == CRC_INTEL)
-	{
-		LayerAt(layer, attr, 14+46, row) = eventAttr[_hot][0];
-		LayerAt(layer, attr, 14+47, row) = eventAttr[_hot][1];
-		LayerAt(layer, attr, 14+48, row) = eventAttr[_hot][2];
-	}
-	else if((Shm->Proc.Features.Info.Vendor.CRC == CRC_AMD)
-	     || (Shm->Proc.Features.Info.Vendor.CRC == CRC_HYGON))
-	{
-		LayerAt(layer, attr, 14+42, row) = eventAttr[_hot][0];
-		LayerAt(layer, attr, 14+43, row) = eventAttr[_hot][1];
-		LayerAt(layer, attr, 14+44, row) = eventAttr[_hot][2];
-	}
-	LayerAt(layer, attr, 14+61, row) = eventAttr[_tmp][0];
-	LayerAt(layer, attr, 14+62, row) = eventAttr[_tmp][1];
-	LayerAt(layer, attr, 14+63, row) = eventAttr[_tmp][2];
+	LayerAt(layer, attr,
+		Draw.Area.Footer.VoltTemp.Hot[1] + 1, row) = eventAttr[_hot][1];
+
+	LayerAt(layer, attr,
+		Draw.Area.Footer.VoltTemp.Hot[1] + 2, row) = eventAttr[_hot][2];
+
+	LayerAt(layer, attr,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 8, row) = eventAttr[_tmp][0];
+
+	LayerAt(layer, attr,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 9, row) = eventAttr[_tmp][1];
+
+	LayerAt(layer, attr,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 10, row)= eventAttr[_tmp][2];
 
 	Draw_Footer_Voltage_Temp[Setting.fahrCels](PFlop);
 
-	LayerAt(layer, code, 75, row) = Buffer[0];
-	LayerAt(layer, code, 76, row) = Buffer[1];
-	LayerAt(layer, code, 77, row) = Buffer[2];
+	LayerAt(layer, code,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 8, row) = Buffer[0];
 
-	LayerAt(layer, code, 67, row) = Buffer[3];
-	LayerAt(layer, code, 68, row) = Buffer[4];
-	LayerAt(layer, code, 69, row) = Buffer[5];
-	LayerAt(layer, code, 70, row) = Buffer[6];
+	LayerAt(layer, code,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 9, row) = Buffer[1];
+
+	LayerAt(layer, code,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 10, row) = Buffer[2];
+
+	LayerAt(layer, code, Draw.Area.Footer.VoltTemp.Hot[0], row) = Buffer[3];
+
+	LayerAt(layer, code,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 1, row) = Buffer[4];
+
+	LayerAt(layer, code,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 2, row) = Buffer[5];
+
+	LayerAt(layer, code,
+		Draw.Area.Footer.VoltTemp.Hot[0] + 3, row) = Buffer[6];
 
 	if (BITWISEAND(LOCKLESS, Shm->SysGate.Operation, 0x1)
 	&& (Shm->SysGate.tickStep == Shm->SysGate.tickReset)) {
 		if ((Draw.Cache.TaskCount != Shm->SysGate.taskCount)
-		 || (Draw.Cache.FreeRAM != Shm->SysGate.memInfo.freeram)) {
+		 || (Draw.Cache.FreeRAM != Shm->SysGate.memInfo.freeram))
+		{
 			Draw.Cache.TaskCount = Shm->SysGate.taskCount;
 			Draw.Cache.FreeRAM = Shm->SysGate.memInfo.freeram;
 

corefreq-cli-rsc-en.h
@@ -533,8 +533,7 @@
 	'T','U','R','B','O',',','C','1','E',',',			\
 	' ','P','M',',','C','3','A',',','C','1','A',',',		\
 	'C','3','U',',','C','1','U',',',				\
-	'T','M',',','H','O','T',']',' ',				\
-	'V','[',' ','.',' ',' ',']',' ','T','[',' ',' ',' ',' ',']' 	\
+	'T','M',',','H','O','T',']',' ' 				\
 }
 
 #define RSC_LAYOUT_FOOTER_TECH_AMD_CODE_EN				\
@@ -542,8 +541,12 @@
 	'S','M','T',',','C','n','Q',',','H','W','P',',',		\
 	'B','O','O','S','T',',','C','1','E',',','C','C','6',		\
 	',','P','C','6',',','C','C','x',',','D','T','S',',',		\
-	'T','T','P',',','H','O','T',']',' ',' ',' ',' ',' ',		\
-	'V','[',' ','.',' ',' ',']',' ','T','[',' ',' ',' ',' ',']' 	\
+	'T','T','P',',','H','O','T',']',' ',' ',' ',' ',' '		\
+}
+
+#define RSC_LAYOUT_FOOTER_VOLT_TEMP_CODE_EN				\
+{									\
+	'V','[',' ','.',' ',' ',']',' ','T','[',' ',' ',' ',' ',']'	\
 }
 
 #define RSC_LAYOUT_FOOTER_SYSTEM_CODE_EN				\

corefreq-cli-rsc-fr.h
@@ -274,6 +274,8 @@ do echo -en "$h$l\t""\xc3""\x$h$l""\t"; done; done;echo
 
 #define RSC_LAYOUT_FOOTER_TECH_AMD_CODE_FR RSC_LAYOUT_FOOTER_TECH_AMD_CODE_EN
 
+#define RSC_LAYOUT_FOOTER_VOLT_TEMP_CODE_FR RSC_LAYOUT_FOOTER_VOLT_TEMP_CODE_EN
+
 #define RSC_LAYOUT_FOOTER_SYSTEM_CODE_FR				\
 {									\
 	'T',0xa2,'c','h','e','s','[',' ',' ',' ',' ',' ',' ',']',	\

corefreq-cli-rsc-theme-dflt.h
@@ -1098,8 +1098,7 @@
 	HDK,HDK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,			\
 	HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,		\
 	HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,				\
-	HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK,				\
-	LWK,HDK,HWK,HWK,HWK,HWK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,HDK 	\
+	HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK 				\
 }
 
 #define RSC_LAYOUT_FOOTER_TECH_AMD_THM_DFLT_ATTR			\
@@ -1107,8 +1106,12 @@
 	HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,		\
 	HDK,HDK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,		\
 	LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,		\
-	HDK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,		\
-	LWK,HDK,HWK,HWK,HWK,HWK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,HDK 	\
+	HDK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK		\
+}
+
+#define RSC_LAYOUT_FOOTER_VOLT_TEMP_THM_DFLT_ATTR			\
+{									\
+	LWK,HDK,HWK,HWK,HWK,HWK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,HDK	\
 }
 
 #define RSC_LAYOUT_FOOTER_SYSTEM_THM_DFLT_ATTR				\

corefreq-cli-rsc-theme-usr1.h
@@ -1098,8 +1098,7 @@
 	HKW,HKW,HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,			\
 	HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,		\
 	HKW,HKW,LKW,HKW,HKW,HKW,HKW,LKW,				\
-	HKW,HKW,LKW,HKW,HKW,HKW,HKW,HKW,				\
-	LKW,HKW,LKW,LKW,LKW,LKW,HKW,HKW,LKW,HKW,HKW,HKW,HKW,LKW,HKW 	\
+	HKW,HKW,LKW,HKW,HKW,HKW,HKW,HKW 				\
 }
 
 #define RSC_LAYOUT_FOOTER_TECH_AMD_THM_USR1_ATTR			\
@@ -1107,8 +1106,12 @@
 	HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,		\
 	HKW,HKW,HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,HKW,HKW,HKW,		\
 	LKW,HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,HKW,HKW,HKW,LKW,		\
-	HKW,HKW,HKW,LKW,HKW,HKW,HKW,HKW,HKW,HKW,HKW,HKW,HKW,		\
-	LKW,HKW,LKW,LKW,LKW,LKW,HKW,HKW,LKW,HKW,LKW,LKW,LKW,LKW,HKW 	\
+	HKW,HKW,HKW,LKW,HKW,HKW,HKW,HKW,HKW,HKW,HKW,HKW,HKW		\
+}
+
+#define RSC_LAYOUT_FOOTER_VOLT_TEMP_THM_USR1_ATTR			\
+{									\
+	LKW,HKW,LKW,LKW,LKW,LKW,HKW,HKW,LKW,HKW,HKW,HKW,HKW,LKW,HKW	\
 }
 
 #define RSC_LAYOUT_FOOTER_SYSTEM_THM_USR1_ATTR				\

corefreq-cli-rsc-theme-usr2.h
@@ -1098,8 +1098,7 @@
 	HDK,HDK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,			\
 	HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,		\
 	HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,				\
-	HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK,				\
-	LWK,HDK,HWK,HWK,HWK,HWK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,HDK 	\
+	HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK 				\
 }
 
 #define RSC_LAYOUT_FOOTER_TECH_AMD_THM_USR2_ATTR			\
@@ -1107,8 +1106,12 @@
 	HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,		\
 	HDK,HDK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,		\
 	LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,HDK,HDK,HDK,LWK,		\
-	HDK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,		\
-	LWK,HDK,HWK,HWK,HWK,HWK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,HDK 	\
+	HDK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK,HDK		\
+}
+
+#define RSC_LAYOUT_FOOTER_VOLT_TEMP_THM_USR2_ATTR			\
+{									\
+	LWK,HDK,HWK,HWK,HWK,HWK,HDK,HDK,LWK,HDK,HDK,HDK,HDK,LWK,HDK	\
 }
 
 #define RSC_LAYOUT_FOOTER_SYSTEM_THM_USR2_ATTR				\

corefreq-cli-rsc.c
@@ -179,6 +179,7 @@ DEF_LDA(RSC_LAYOUT_RULER_SLICE);
 DEF_LDA(RSC_LAYOUT_FOOTER_TECH_X86);
 DEF_LDA(RSC_LAYOUT_FOOTER_TECH_INTEL);
 DEF_LDA(RSC_LAYOUT_FOOTER_TECH_AMD);
+DEF_LDA(RSC_LAYOUT_FOOTER_VOLT_TEMP);
 DEF_LDA(RSC_LAYOUT_FOOTER_SYSTEM);
 #endif /* NO_FOOTER */
 
@@ -385,6 +386,7 @@ RESOURCE_ST Resource[] = {
 	LDA(RSC_LAYOUT_FOOTER_TECH_X86),
 	LDA(RSC_LAYOUT_FOOTER_TECH_INTEL),
 	LDA(RSC_LAYOUT_FOOTER_TECH_AMD),
+	LDA(RSC_LAYOUT_FOOTER_VOLT_TEMP),
 	LDA(RSC_LAYOUT_FOOTER_SYSTEM),
 	LDQ(RSC_LAYOUT_FOOTER_TSC_NONE),
 	LDQ(RSC_LAYOUT_FOOTER_TSC_VAR),

corefreq-cli-rsc.h
@@ -208,6 +208,7 @@ enum {
 	RSC_LAYOUT_FOOTER_TECH_X86,
 	RSC_LAYOUT_FOOTER_TECH_INTEL,
 	RSC_LAYOUT_FOOTER_TECH_AMD,
+	RSC_LAYOUT_FOOTER_VOLT_TEMP,
 	RSC_LAYOUT_FOOTER_SYSTEM,
 	RSC_LAYOUT_FOOTER_TSC_NONE,
 	RSC_LAYOUT_FOOTER_TSC_VAR,

commit 1f202abeaa3291172c3f4ff398c37ade30a99d85

Fix to UI regression introduced by commit 6a22bbda1b3671a0a6f61f04b0075a96f5a3da4e

corefreq-cli.c
@@ -14068,8 +14068,8 @@ void Layout_Footer(Layer *layer, CUINT row)
 	hTech0.code[11] = TSC[Shm->Proc.Features.InvariantTSC].code[5];
 	hTech0.code[12] = TSC[Shm->Proc.Features.InvariantTSC].code[6];
 
-	hTech0.attr[ 6] = hTech0.attr[ 7]=hTech0.attr[ 8] =
-	hTech0.attr[ 9] = hTech0.attr[10]=hTech0.attr[11] =
+	hTech0.attr[ 6] = hTech0.attr[ 7] = hTech0.attr[ 8] = \
+	hTech0.attr[ 9] = hTech0.attr[10] = hTech0.attr[11] = \
 	hTech0.attr[12] = TSC[Shm->Proc.Features.InvariantTSC].attr;
 
 	LayerCopyAt(	layer, hTech0.origin.col, hTech0.origin.row,
@@ -14137,7 +14137,7 @@ void Layout_Footer(Layer *layer, CUINT row)
 			hSpace,
 			RSC(UI).ATTR()[UI_LAYOUT_FOOTER_FILL]);
 
-	Draw.Area.Footer.VoltTemp.Hot[1] = Draw.Area.Footer.VoltTemp.Hot[0] - 7;
+	Draw.Area.Footer.VoltTemp.Hot[1] = hTech1.origin.col + 46;
     }
     else
     {
@@ -14187,7 +14187,7 @@ void Layout_Footer(Layer *layer, CUINT row)
 			hSpace,
 			RSC(UI).ATTR()[UI_LAYOUT_FOOTER_FILL]);
 
-	Draw.Area.Footer.VoltTemp.Hot[1]=Draw.Area.Footer.VoltTemp.Hot[0] - 11;
+	Draw.Area.Footer.VoltTemp.Hot[1] = hTech1.origin.col + 42;
       }
     }
 	LayerCopyAt(layer, hVoltTemp0.origin.col, hVoltTemp0.origin.row,