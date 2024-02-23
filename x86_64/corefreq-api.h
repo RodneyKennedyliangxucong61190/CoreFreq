RodneyKennedyliangxucong61190/CoreFreq

commit 896433bf8f0ba1f32c675511e73a58212aad2030

[AMD] Added HWCR to System Registers

x86_64/corefreq-api.h
@@ -444,6 +444,7 @@ typedef struct
 		Bit64			XCR0	__attribute__ ((aligned (8)));
 		Bit64			XSS	__attribute__ ((aligned (8)));
 		Bit64			SYSCFG	__attribute__ ((aligned (8)));
+		HWCR			HWCR	__attribute__ ((aligned (8)));
 	} SystemRegister;
 
 	unsigned int			Bind;

x86_64/corefreqd.c
@@ -7434,6 +7434,9 @@ void SystemRegisters(	RO(SHM_STRUCT) *RO(Shm), RO(CORE) **RO(Core),
 
 	RO(Shm)->Cpu[cpu].SystemRegister.SYSCFG = \
 				RO(Core, AT(cpu))->SystemRegister.SYSCFG;
+
+	RO(Shm)->Cpu[cpu].SystemRegister.HWCR = \
+				RO(Core, AT(cpu))->SystemRegister.HWCR.value;
 }
 
 void SysGate_OS_Driver(RO(SHM_STRUCT) *RO(Shm), RO(PROC) *RO(Proc))

commit 056f28833276ddbca8a8c9d9bcf996ec0faf2b94

[AMD] Fix HWCR crash regression introduced by commit 896433bf8f0ba1f32c675511e73a58212aad2030 [Intel][Goldmont] Adding the Host Bridge PCI id [CR] Cleanup non ascii characters in comments

x86_64/corefreq-api.h
@@ -1153,6 +1153,8 @@ typedef struct
 #define DID_INTEL_SLM_PTR		0x0f00
 /* Source: [Atom/Airmont] Pentium(R) CPU N3700				*/
 #define DID_INTEL_AIRMONT_HB		0x2280
+/* Source: Intel(R) Celeron(R) CPU J3455 @ 1.50GHz			*/
+#define DID_INTEL_GOLDMONT_HB		0x5af0
 /* Source: /include/linux/pci_ids.h					*/
 #define DID_INTEL_I7_MCR		0x2c18
 #define DID_INTEL_I7_MC_CH0_CTRL	0x2c20
@@ -1198,14 +1200,14 @@ typedef struct
 #define DID_INTEL_IIO_CORE_REG		0x3728
 /* Source: /include/linux/pci_ids.h					*/
 #define DID_INTEL_SNB_IMC_HA0		0x3ca0
-/* Source: 2nd Generation Intel® Core™ Processor Family Vol2		*/
+/* Source: 2nd Generation Intel Core Processor Family Vol2		*/
 #define DID_INTEL_SNB_IMC_SA		0x0100
 #define DID_INTEL_SNB_IMC_0104		0x0104
 /* Source: /drivers/edac/sb_edac.c					*/
 /*TODO( Harware missing )*/
 #define DID_INTEL_IVB_IMC_HA0		0x0ea0
 #define DID_INTEL_IVB_IMC_HA1		0x0e60
-/* Source: 3rd Generation Intel® Core™ Processor Family Vol2		*/
+/* Source: 3rd Generation Intel Core Processor Family Vol2		*/
 #define DID_INTEL_IVB_IMC_SA		0x0150
 #define DID_INTEL_IVB_IMC_0154		0x0154
 /* Source: Intel Xeon Processor E5 & E7 v1 Datasheet Vol 2		*/
@@ -1329,7 +1331,7 @@ typedef struct
 /*	Power Control Unit						*/
 /*	PCU: Device=30 - Function=3					*/
 #define DID_INTEL_HSW_EP_CAPABILITY	0x2fc0
-/* Source: 4th, 5th Generation Intel® Core™ Processor Family Vol2 §3.0	*/
+/* Source: 4th, 5th Generation Intel Core Processor Family Vol2 Chap. 3.0 */
 #define DID_INTEL_HASWELL_IMC_HA0	0x2fa0
 #define DID_INTEL_HASWELL_IMC_SA	0x0c00
 #define DID_INTEL_HASWELL_MH_IMC_HA0	0x0c04
@@ -1338,18 +1340,18 @@ typedef struct
 #define DID_INTEL_BROADWELL_D_IMC_HA0	0x1610
 #define DID_INTEL_BROADWELL_H_IMC_HA0	0x1614
 #define DID_INTEL_BROADWELL_U_IMC_HA0	0x1618
-/* Source: 6th Generation Intel® Processor Datasheet for U/Y-Platforms Vol2 */
+/* Source: 6th Generation Intel Processor Datasheet for U/Y-Platforms Vol2 */
 #define DID_INTEL_SKYLAKE_U_IMC_HA	0x1904
 #define DID_INTEL_SKYLAKE_Y_IMC_HA	0x190c
-/* Source: 6th Generation Intel® Processor Datasheet for S-Platforms Vol2 */
+/* Source: 6th Generation Intel Processor Datasheet for S-Platforms Vol2 */
 #define DID_INTEL_SKYLAKE_S_IMC_HAD	0x190f
 #define DID_INTEL_SKYLAKE_S_IMC_HAQ	0x191f
-/* Source: 6th Generation Intel® Processor Datasheet for H-Platforms Vol2 */
+/* Source: 6th Generation Intel Processor Datasheet for H-Platforms Vol2 */
 #define DID_INTEL_SKYLAKE_H_IMC_HAD	0x1900
 #define DID_INTEL_SKYLAKE_H_IMC_HAQ	0x1910
 /* Source: Intel Xeon Processor E3-1200 v5 Product Family		*/
 #define DID_INTEL_SKYLAKE_DT_IMC_HA	0x1918
-/* Source:7th Generation Intel® Processor for S-Platforms & Core X-Series Vol2*/
+/* Source:7th Generation Intel Processor for S-Platforms & Core X-Series Vol2*/
 #define DID_INTEL_KABYLAKE_H_IMC_HAD	0x5900
 #define DID_INTEL_KABYLAKE_U_IMC_HA	0x5904
 #define DID_INTEL_KABYLAKE_Y_IMC_HA	0x590c
@@ -1360,10 +1362,10 @@ typedef struct
 #define DID_INTEL_KABYLAKE_U_IMC_HAQ	0x5914
 #define DID_INTEL_KABYLAKE_S_IMC_HAQ	0x591f
 #define DID_INTEL_KABYLAKE_X_IMC_HAQ	0x5906
-/* Source: 8th Generation Intel® Processor for S-Platforms Datasheet Vol2 */
+/* Source: 8th Generation Intel Processor for S-Platforms Datasheet Vol2 */
 #define DID_INTEL_COFFEELAKE_S_IMC_HAQ	0x3e1f
 #define DID_INTEL_COFFEELAKE_S_IMC_HAS	0x3ec2
-/* Source: 8th and 9th Generation Intel® Core™ and Xeon™ E Processor Families */
+/* Source: 8th and 9th Generation Intel Core and Xeon E Processor Families */
 #define DID_INTEL_COFFEELAKE_R_S_IMC_HAD 0x3e0f
 #define DID_INTEL_COFFEELAKE_R_U_IMC_HAD 0x3ecc
 #define DID_INTEL_COFFEELAKE_R_U_IMC_HAQ 0x3ed0
@@ -1376,7 +1378,7 @@ typedef struct
 #define DID_INTEL_COFFEELAKE_R_S_IMC_HAQ 0x3e33
 #define DID_INTEL_COFFEELAKE_R_S_IMC_HAS 0x3eca
 #define DID_INTEL_COFFEELAKE_R_S_IMC_HAO 0x3e32
-/* Source: 8th Generation Intel® Core™ Processor Families Datasheet Vol2 */
+/* Source: 8th Generation Intel Core Processor Families Datasheet Vol2	*/
 #define DID_INTEL_WHISKEYLAKE_U_IMC_HAD 0x3e35
 #define DID_INTEL_WHISKEYLAKE_U_IMC_HAQ 0x3e34
 #define DID_INTEL_CANNONLAKE_U_IMC_HB	0x5a04
@@ -1413,7 +1415,7 @@ typedef struct
 #define DID_INTEL_TIGERLAKE_U3_IMC	0x9a12
 #define DID_INTEL_TIGERLAKE_U4_IMC	0x9a14
 #define DID_INTEL_TIGERLAKE_H_IMC	0x9a36
-/* Source: 11th Generation Intel® Core™ Processor Desktop Datasheet Vol1 */
+/* Source: 11th Generation Intel Core Processor Desktop Datasheet Vol1	*/
 #define DID_INTEL_TIGERLAKE_UP3_IMC	0xa082
 #define DID_INTEL_TIGERLAKE_UP4_IMC	0xa087
 #define DID_INTEL_ROCKETLAKE_S_8C_IMC_HB 0x4c43

x86_64/corefreqd.c
@@ -37,7 +37,7 @@
 	sysconf(_SC_PAGESIZE) > 0 ? sysconf(_SC_PAGESIZE) : 4096	\
 )
 
-/* §8.10.6.7 Place Locks and Semaphores in Aligned, 128-Byte Blocks of Memory */
+/*Ch:8.10.6.7 Place Locks and Semaphores in Aligned, 128-Byte Blocks of Memory*/
 static BitCC roomSeed	__attribute__ ((aligned (16))) = InitCC(0x0);
 static BitCC roomCore	__attribute__ ((aligned (16))) = InitCC(0x0);
 static BitCC roomClear	__attribute__ ((aligned (16))) = InitCC(0x0);
@@ -388,7 +388,7 @@ static void (*ComputeVoltage_None_Matrix[4])(	struct FLIP_FLOP*,
 static void ComputeVoltage_Intel_Core2( struct FLIP_FLOP *CFlip,
 						RO(SHM_STRUCT) *RO(Shm),
 						unsigned int cpu )
-{	/* Intel Core 2 Extreme Datasheet §3.3-Table 2			*/
+{	/* Intel Core 2 Extreme Datasheet Chap. 3.3-Table 2		*/
 	COMPUTE_VOLTAGE(INTEL_CORE2,
 			CFlip->Voltage.Vcore,
 			CFlip->Voltage.VID);
@@ -608,7 +608,7 @@ static void (*ComputeVoltage_AMD_Matrix[4])( struct FLIP_FLOP*,
 static void ComputeVoltage_AMD_0Fh( struct FLIP_FLOP *CFlip,
 						RO(SHM_STRUCT) *RO(Shm),
 						unsigned int cpu )
-{	/* AMD BKDG Family 0Fh §10.6 Table 70				*/
+{	/* AMD BKDG Family 0Fh Chap 10.6 Table 70			*/
 	COMPUTE_VOLTAGE(AMD_0Fh,
 			CFlip->Voltage.Vcore,
 			CFlip->Voltage.VID);
@@ -8206,7 +8206,7 @@ static void Pkg_ComputeVoltage_Intel_SoC(struct PKG_FLIP_FLOP *PFlip)
 }
 
 static void Pkg_ComputeVoltage_Intel_SNB(struct PKG_FLIP_FLOP *PFlip)
-{	/* Intel 2nd Generation Datasheet Vol-1 §7.4 Table 7-1		*/
+{	/* Intel 2nd Generation Datasheet Vol-1 Chap. 7.4 Table 7-1	*/
 	COMPUTE_VOLTAGE(INTEL_SNB,
 			PFlip->Voltage.CPU,
 			PFlip->Voltage.VID.CPU);

x86_64/corefreqk.c
@@ -1891,7 +1891,7 @@ static CLOCK BaseClock_Skylake(unsigned int ratio)
 };
 
 static CLOCK BaseClock_AMD_Family_17h(unsigned int ratio)
-{	/* Source: AMD PPR Family 17h § 1.4/ Table 11: REFCLK = 100 MHz */
+{	/* Source: AMD PPR Family 17h Chap. 1.4/ Table 11: REFCLK = 100 MHz */
 	CLOCK clock = {.Q = 100, .R = 0, .Hz = 100000000L};
 	UNUSED(ratio);
 	return clock;
@@ -2327,7 +2327,7 @@ static void Map_AMD_Topology(void *arg)
 /*
  Enumerate the topology of Processors, Cores and Threads
  Remark: Early single-core processors are not processed.
- Sources: Intel Software Developer's Manual vol 3A §8.9 /
+ Sources: Intel Software Developer's Manual vol 3A Chap. 8.9 /
 	  Intel whitepaper: Detecting Hyper-Threading Technology /
 */
 unsigned short FindMaskWidth(unsigned short maxCount)
@@ -2717,7 +2717,7 @@ void Intel_Core_Platform_Info(unsigned int cpu)
 	}
 
 	RDMSR(PerfStatus, MSR_IA32_PERF_STATUS);
-	if (PerfStatus.value != 0) {				/* §18.18.3.4 */
+	if (PerfStatus.value != 0) {			/* Chap. 18.18.3.4 */
 		if (PerfStatus.CORE.XE_Enable) {
 			ratio1 = PerfStatus.CORE.MaxBusRatio;
 		} else {
@@ -4743,7 +4743,7 @@ void Query_G965(void __iomem *mchmap, unsigned short mc)
 }
 
 void Query_P35(void __iomem *mchmap, unsigned short mc)
-{	/* Source: Intel® 3 Series Express Chipset Family. */
+{	/* Source: Intel 3 Series Express Chipset Family.		*/
 	unsigned short cha;
 
 	PUBLIC(RO(Proc))->Uncore.Bus.ClkCfg.value = readl(mchmap + 0xc00);
@@ -4944,8 +4944,8 @@ inline void BIOS_DDR(void __iomem *mchmap)
 }
 
 void Query_SNB_IMC(void __iomem *mchmap, unsigned short mc)
-{	/* Sources:	2nd & 3rd Generation Intel® Core™ Processor Family
-			Intel® Xeon Processor E3-1200 Family		*/
+{	/* Sources:	2nd & 3rd Generation Intel Core Processor Family
+			Intel Xeon Processor E3-1200 Family		*/
 	unsigned short cha, dimmCount[2];
 
 	PUBLIC(RO(Proc))->Uncore.Bus.ClkCfg.value = readl(mchmap + 0xc00);
@@ -5073,7 +5073,7 @@ void Query_Turbo_TDP_Config(void __iomem *mchmap)
 }
 
 void Query_HSW_IMC(void __iomem *mchmap, unsigned short mc)
-{	/*Source: Desktop 4th & 5th Generation Intel® Core™ Processor Family.*/
+{	/* Source: Desktop 4th & 5th Generation Intel Core Processor Family */
 	unsigned short cha, dimmCount[2];
 
 	PUBLIC(RO(Proc))->Uncore.Bus.ClkCfg.value = readl(mchmap + 0xc00);
@@ -5139,7 +5139,7 @@ inline void SKL_SA(void __iomem *mchmap)
 }
 
 void Query_SKL_IMC(void __iomem *mchmap, unsigned short mc)
-{	/*Source: 6th & 7th Generation Intel® Processor for S-Platforms Vol 2*/
+{	/* Source: 6th & 7th Generation Intel Processor for S-Platforms Vol 2*/
 	unsigned short cha;
 	/*		Intra channel configuration			*/
 	PUBLIC(RO(Proc))->Uncore.MC[mc].SKL.MADCH.value = readl(mchmap+0x5000);
@@ -5207,7 +5207,7 @@ inline void RKL_SA(void __iomem *mchmap)
 }
 
 void Query_RKL_IMC(void __iomem *mchmap, unsigned short mc)
-{	/*Source: 11th Generation Intel® Core Processor Datasheet Vol 2*/
+{	/* Source: 11th Generation Intel Core Processor Datasheet Vol 2 */
 	unsigned short cha;
 	/*		Intra channel configuration			*/
 	PUBLIC(RO(Proc))->Uncore.MC[mc].RKL.MADCH.value = readl(mchmap+0x5000);
@@ -5276,7 +5276,7 @@ void Query_RKL_IMC(void __iomem *mchmap, unsigned short mc)
 #define TGL_SA	RKL_SA
 
 void Query_TGL_IMC(void __iomem *mchmap, unsigned short mc)
-{	/*Source: 11th Generation Intel® Core Processor Datasheet Vol 2 */
+{	/* Source: 11th Generation Intel Core Processor Datasheet Vol 2 */
 	unsigned short cha;
 
 	PUBLIC(RO(Proc))->Uncore.MC[mc].ChannelCount = 0;
@@ -5367,7 +5367,7 @@ EXIT_TGL_IMC:
 #define ADL_SA	TGL_SA
 
 void Query_ADL_IMC(void __iomem *mchmap, unsigned short mc)
-{	/*Source: 12th Generation Intel® Core Processor Datasheet Vol 2 */
+{	/* Source: 12th Generation Intel Core Processor Datasheet Vol 2 */
 	unsigned short cha;
 
 	PUBLIC(RO(Proc))->Uncore.MC[mc].ChannelCount = 0;
@@ -5460,7 +5460,13 @@ EXIT_ADL_IMC:
 }
 
 void Query_GLK_IMC(void __iomem *mchmap, unsigned short mc)
-{ /* Source: Intel® Pentium® Silver and Intel® Celeron® Processors Vol 2 */
+{ /* Source: Intel Pentium Silver and Intel Celeron Processors Vol 2	*/
+	PUBLIC(RO(Proc))->Uncore.MC[mc].ChannelCount = \
+	PUBLIC(RO(Proc))->Uncore.MC[mc].SlotCount = 0;
+}
+
+void Query_GDM_IMC(void __iomem *mchmap, unsigned short mc)
+{ /* Source: Intel Pentium and Celeron Processor N- and J- Series Vol 2 */
 	PUBLIC(RO(Proc))->Uncore.MC[mc].ChannelCount = \
 	PUBLIC(RO(Proc))->Uncore.MC[mc].SlotCount = 0;
 }
@@ -6358,6 +6364,13 @@ static PCI_CALLBACK GLK_IMC(struct pci_dev *dev)
 	SoC_SKL_VTD();
 
 	return Router(dev, 0x48, 64, 0x8000, Query_GLK_IMC, 0);
+}
+
+static PCI_CALLBACK GDM_IMC(struct pci_dev *dev)
+{
+	PUBLIC(RO(Proc))->Uncore.CtrlCount = 0;
+
+	return Router(dev, 0x48, 64, 0x8000, Query_GDM_IMC, 0);
 }
 
 static PCI_CALLBACK AMD_0Fh_MCH(struct pci_dev *dev)
@@ -7428,7 +7441,7 @@ static void Query_AuthenticAMD(unsigned int cpu)
 }
 
 unsigned short Compute_AMD_Family_0Fh_Boost(unsigned int cpu)
-{	/* Source: BKDG for AMD NPT Family 0Fh: §13.8			*/
+{	/* Source: BKDG for AMD NPT Family 0Fh: Chap. 13.8			*/
 	unsigned short SpecTurboRatio = 0;
 
     if (PUBLIC(RO(Proc))->Features.AdvPower.EDX.FID == 1)
@@ -7971,10 +7984,11 @@ static void TurboClock_AMD_Zen_PerCore(void *arg)
 	PSTATEDEF PstateDef = {.value = 0};
 	COF_ST COF = {.Q = 0, .R = 0};
 	unsigned int FID, DID;
-	const unsigned int cpu = pClockZen->pClockMod->cpu;
+	const unsigned int _cpu = pClockZen->pClockMod->cpu == -1 ?
+		PUBLIC(RO(Proc))->Service.Core : pClockZen->pClockMod->cpu;
 	/*	Make sure the Core Performance Boost is disabled.	*/
-	RDMSR(PUBLIC(RO(Core, AT(cpu)))->SystemRegister.HWCR, MSR_K7_HWCR);
-  if (PUBLIC(RO(Core, AT(cpu)))->SystemRegister.HWCR.Family_17h.CpbDis)
+	RDMSR(PUBLIC(RO(Core, AT(_cpu)))->SystemRegister.HWCR, MSR_K7_HWCR);
+  if (PUBLIC(RO(Core, AT(_cpu)))->SystemRegister.HWCR.Family_17h.CpbDis)
   {
 	/*	Apply if and only if the P-State is enabled ?		*/
 	RDMSR(PstateDef, pClockZen->PstateAddr);

x86_64/corefreqk.h
@@ -1145,7 +1145,7 @@ typedef struct
 
 
 /* Sources:
- * Intel® 64 and IA-32 Architectures Software Developer’s Manual; Vol. 2A
+ * Intel 64 and IA-32 Architectures Software Developer’s Manual; Vol. 2A
  * AMD64 Architecture Programmer’s Manual; Vol. 3
 */
 
@@ -2121,6 +2121,7 @@ static PCI_CALLBACK TGL_IMC(struct pci_dev *dev) ;
 static PCI_CALLBACK ADL_IMC(struct pci_dev *dev) ;
 #define ADL_PCH CML_PCH
 static PCI_CALLBACK GLK_IMC(struct pci_dev *dev) ;
+static PCI_CALLBACK GDM_IMC(struct pci_dev *dev) ;
 #define RPL_IMC ADL_IMC
 #define RPL_PCH CML_PCH
 #define MTL_IMC ADL_IMC
@@ -2241,6 +2242,10 @@ static struct pci_device_id PCI_SoC_ids[] = {
 	{	/* Atom - Airmont					*/
 		PCI_VDEVICE(INTEL, DID_INTEL_AIRMONT_HB),
 		.driver_data = (kernel_ulong_t) SoC_AMT
+	},
+	{	/* Goldmont						*/
+		PCI_VDEVICE(INTEL, DID_INTEL_GOLDMONT_HB),
+		.driver_data = (kernel_ulong_t) GDM_IMC
 	},
 	{0, }
 };
@@ -9598,7 +9603,7 @@ static ARCH Arch[ARCHITECTURES] = {
 	.thermalFormula = THERMAL_FORMULA_INTEL,
 	.voltageFormula = VOLTAGE_FORMULA_INTEL_SNB,
 	.powerFormula   = POWER_FORMULA_INTEL_ATOM,
-	.PCI_ids = PCI_Void_ids,
+	.PCI_ids = PCI_SoC_ids,
 	.Uncore = {
 		.Start = NULL,
 		.Stop = NULL,

x86_64/intel_reg.h
@@ -1978,7 +1978,7 @@ typedef struct
 		ReservedBits2	: 64-12;
 } MTRR_DEF_TYPE;
 
-	§18.2.4 PM4
+	Chap. 18.2.4 PM4
 
 	IA32_PERF_GLOBAL_STATUS_RESET
 	IA32_PERF_GLOBAL_STATUS_SET
@@ -3555,7 +3555,7 @@ typedef union
 		ReservedBits2	: 23-15,
 		VT_d		: 24-23, /* VT-d: 0=Enable, 1=Disable	*/
 		ReservedBits3	: 32-24;
-	};	/* §2.5.33 CAPID0_A Capabilities A Register		*/
+	};	/* Chap. 2.5.33 CAPID0_A Capabilities A Register	*/
 	struct {
 		unsigned int
 		DDR3L_EN	:  1-0,  /* DDR3L (1.35V) operation allowed */
@@ -3773,7 +3773,7 @@ typedef union
 		SMTCAP		: 29-28, /* SMT Capability		*/
 		ReservedBits5	: 32-29;
 	};
-} IVB_CAPID_B;	/* §2.5.39 CAPID0_B Capabilities B Register		*/
+} IVB_CAPID_B;	/* Chap. 2.5.39 CAPID0_B Capabilities B Register	*/
 
 typedef union
 {	/* Device: 0 - Function: 0 - Offset Channel0: 4C00h		*/
@@ -4719,7 +4719,7 @@ typedef union
 		ReservedBits3	: 25-24,
 		ECCDIS		: 26-25, /* 0:ECC capable, 1:Not ECC capable */
 		ReservedBits4	: 32-26;
-	};	/* §3.39 CAPID0_A Capabilities A Register		*/
+	};	/* Chap. 3.39 CAPID0_A Capabilities A Register		*/
 } SKL_CAPID_A;
 
 typedef union
@@ -4753,7 +4753,7 @@ typedef union
 		PEG11D		: 30-29,
 		PEG12D		: 31-30,
 		NVME_FOD	: 32-31; /* 1: Disable NVMe at Dev 3 Func 0 */
-	};	/* RKL: §3.1.38 ; TGL: $3.1.40				*/
+	};	/* RKL: Chap. 3.1.38 ; TGL: $3.1.40			*/
 } RKL_CAPID_A;
 
 #define TGL_CAPID_A	RKL_CAPID_A
@@ -4820,7 +4820,7 @@ typedef union
 		SMTCAP		: 29-28,
 		ReservedBits8	: 31-29,
 		IMGU_DIS	: 32-31; /* Device 5 associated memory spaces */
-	};	/* §3.40 CAPID0_B Capabilities B Register		*/
+	};	/* Chap. 3.40 CAPID0_B Capabilities B Register		*/
 } SKL_CAPID_B;
 
 typedef union
@@ -4852,7 +4852,7 @@ typedef union
 		OC_ENABLED	: 30-29, /* 0: Overclocking is disabled */
 		TRACE_HUB_DIS	: 31-30, /* Trace Hub & I/O are disabled */
 		IMGU_DIS	: 32-31; /* Device 5 associated memory spaces */
-	};	/* RKL: §3.1.39 ; TGL: $3.1.41				*/
+	};	/* RKL: Chap. 3.1.39 ; TGL: $3.1.41			*/
 } RKL_CAPID_B;
 
 #define TGL_CAPID_B	RKL_CAPID_B
@@ -4899,7 +4899,7 @@ typedef union
 		DMFC_LPDDR3	: 17-14,
 		DMFC_DDR4	: 20-17,
 		ReservedBits2	: 32-20;
-	};	/* §3.41 CAPID0_C Capabilities C Register		*/
+	};	/* Chap. 3.41 CAPID0_C Capabilities C Register		*/
 } SKL_CAPID_C;
 
 typedef union
@@ -4921,7 +4921,7 @@ typedef union
 		DATA_RATE_DDR4	: 28-23, /* mult of 266 MHz iff DDR_OVERCLOCK */
 		PEGG4_DIS	: 29-28,
 		ReservedBits2	: 32-29;
-	};	/* RKL: §3.1.40 ; TGL: $3.1.42				*/
+	};	/* RKL: Chap. 3.1.40 ; TGL: $3.1.42			*/
 } RKL_CAPID_C;
 
 #define TGL_CAPID_C	RKL_CAPID_C